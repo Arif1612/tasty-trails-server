84e2c05f090cbb81a4f346121904f58c8b3e6ec4
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-05-27 16:30:06 +0600

    chefs.json data will find port 5005/chefs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5005
+const express = require("express");
+const app = express();
+const port = 5005;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const chefs = require("./data/chefs.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 
 app.listen(port, () => {
-  console.log(`Tasty Trails API is running on ${port}`)
-})
+  console.log(`Tasty Trails API is running on ${port}`);
+});
